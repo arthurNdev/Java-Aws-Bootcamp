150466a69dd23682081c6814c8ffd86b44c00bdb
arthurNdev <arthurnina.dev@gmail.com>
2023-08-18 19:43:14 -0300

    CadastroSistema

---
+++ src/SistemaCadastro.java
@@ -0,0 +1,13 @@
+public class SistemaCadastro {
+
+    public static void main(String[] args) {
+
+        // Criar uma pessoa no sistema
+        Pessoa marcos = new Pessoa("123", "MARCOS");
+
+        // Definir o endereço
+        marcos.setEndereco("RUA MARECHAL");
+
+        System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+    }
+}
